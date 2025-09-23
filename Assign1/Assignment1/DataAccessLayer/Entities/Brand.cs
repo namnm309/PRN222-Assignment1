9f63fc8a08c5590bc555c5b0494bb566f794645f
namnm309 <137751495+namminhs394@users.noreply.github.com>
2025-09-23 15:54:54 +0700

    Update some entities

---
+++ Assign1/Assignment1/DataAccessLayer/Entities/Brand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Brand : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+
+    }
+}
---
+++ Assign1/Assignment1/DataAccessLayer/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Sku { get; set; } // Stock Keeping Unit
+        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public bool IsActive { get; set; } = true;
+
+        //Qhe n-1 với Brand
+        public virtual Brand BrandName { get; set; }
+    }
+}
